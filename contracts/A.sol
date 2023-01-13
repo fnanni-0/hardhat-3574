1c200306d88aadd9ce7a83f9bfbbf0902d007c46
Franco Victorio <victorio.franco@gmail.com>
2023-01-13 10:05:22 +0100

    Initial commit

---
+++ contracts/A.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./C.sol";
+
+contract A {
+  C c;
+  uint public x = 1;
+
+  function inc() public {
+    uint x1 = 1;
+    uint x2 = 1;
+    uint x3 = 1;
+    uint x4 = 1;
+    uint x5 = 1;
+    uint x6 = 1;
+    uint x7 = 1;
+    uint x8 = 1;
+    uint x9 = 1;
+    uint x10 = 1;
+    uint x11 = 1;
+    uint x12 = 1;
+    uint x13 = 1;
+    uint x14 = 1;
+    uint x15 = 1;
+    uint x16 = 1;
+    uint x17 = 1;
+    uint x18 = 1;
+    uint x19 = 1;
+    uint x20 = 1;
+
+    x = x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9 + x10 + x11 + x12 + x13 + x14 + x15 + x16 + x17 + x18 + x19 + x20;
+  }
+}
---
+++ hardhat.config.js
@@ -0,0 +1,31 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.17",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+    overrides: {
+      "contracts/A.sol": {
+        version: "0.8.17",
+        settings: {
+          viaIR: true,
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    },
+  },
+  networks: {
+    hardhat: {},
+  },
+};
